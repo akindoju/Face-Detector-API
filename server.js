32a366019f7fc103cedf5216aaa4980b93cf79fe
akindoju <davidd.akindoju@gmail.com>
2021-12-29 14:54:11 +0100

    Added sessions to face detector app

--- server.js
+++ server.js
@@ -1,38 +1,50 @@
-const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
-const auth = require('./controllers/authorization');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+const auth = require("./controllers/authorization");
 
-//Database Setup - add your own information here based on the DB you created
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+    host: "127.0.0.1",
+    user: "postgres",
+    password: process.env.SERVER_PASSWORD,
+    database: "face-detector1",
+  },
 });
 
 const app = express();
 
-app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+app.use(cors());
+app.use(express.json());
 
-app.post('/signin', signin.signinAuthentication(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
-app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
-app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
+app.get("/", (req, res) => {
+  res.send("Up and Runnnnnning");
+});
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-})
+app.post("/signin", signin.signinAuthentication(db, bcrypt));
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", auth.requireAuth, (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.post("/profile/:id", auth.requireAuth, (req, res) => {
+  profile.handleProfileUpdate(req, res, db);
+});
+app.put("/image", auth.requireAuth, (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post("/imageurl", auth.requireAuth, (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
